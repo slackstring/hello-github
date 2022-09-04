3cff12d8b47e511de825115052b0de151b413d6c
slackstring <slackstring88@gmail.com>
2022-09-04 16:54:26 +0200

    Update auf taschenrechner V1

--- Hello-Github/ConsoleApp1/Program.cs
+++ Hello-Github/ConsoleApp1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+         
+            /* Console.WriteLine("Hello World");
+            Console.WriteLine("Zum Beenden Enter drücken!");
+            */
+            Ausgabe("Taschenrechner für Ganzzahlen");
+            Ausgabe("Wert für x eingeben, mit Enter bestätigen");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Ausgabe("Wert für y eingeben, mit Enter bestätigen");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Ausgabe("Ergebnis der Addition ist " + Sum(x, y));
+        }
+
+        static void Ausgabe(string sentence)
+        {
+            Console.WriteLine(sentence);
+        }
+
+        static int Sum(int x, int y)
+        {
+            int result = x + y;
+            return result;
+        }
+    }
+}
+
---
+++ einfacheAusgabe/einfacheAusgabe/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace einfacheAusgabe
+{
+    class ausgabe
+    {
+        static void Main(string[] args)
+            {
+               Console.WriteLine("Einfache Ausgabe auf der Konsole im Rahmen der Udemy Lektion");
+               Console.ReadLine();
+            }
+
+    }
+}
