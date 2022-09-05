223d141dbdbcf5487529b29fc363d8d9e5134134
slackstring <slackstring88@gmail.com>
2022-09-05 20:52:25 +0200

    update Taschenrechner auf Float// Test Arrays,IfElse // Funktionen

--- Hello-Github/ConsoleApp1/Program.cs
+++ Hello-Github/ConsoleApp1/Program.cs
@@ -17,10 +17,23 @@ namespace Helloworld
             */
             Ausgabe("Taschenrechner für Ganzzahlen");
             Ausgabe("Wert für x eingeben, mit Enter bestätigen");
-            int x = Convert.ToInt32(Console.ReadLine());
+            float x = float.Parse(Console.ReadLine());
             Ausgabe("Wert für y eingeben, mit Enter bestätigen");
-            int y = Convert.ToInt32(Console.ReadLine());
-            Ausgabe("Ergebnis der Addition ist " + Sum(x, y));
+            float y = float.Parse(Console.ReadLine());
+            Ausgabe("1 für Addition / 2 für Subtraktion");
+            int operation = Convert.ToInt32(Console.ReadLine());
+            Ausgabe("Ergebnis ist " + Calc(x, y, operation));
+
+            string[] zeichenkette = new string[6];
+            int[] Zahlen = new int[6];
+            Zahlen[0] = 1;
+            Zahlen[1] = 2;
+            Zahlen[2] = 3;
+            foreach (int counter in Zahlen)
+            {
+                Console.WriteLine(counter);
+            }
+            Console.ReadLine();
         }
 
         static void Ausgabe(string sentence)
@@ -28,10 +41,21 @@ namespace Helloworld
             Console.WriteLine(sentence);
         }
 
-        static int Sum(int x, int y)
+        static float Calc(float x, float y, int operation)
         {
-            int result = x + y;
-            return result;
+            float resultAdd = x + y;
+            float resultSub = x - y;
+            if (operation == 1 )
+            {
+                return resultAdd;
+            } else if (operation == 2)
+            {
+                return resultSub;
+            }else
+            {
+                return 0;
+            }
+          
         }
     }
 }
